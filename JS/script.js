0d15fe1338c7ad4661f3698687135cd0934c1043
Dantas <gdantaslopes@gmail.com>
2023-03-21 01:53:25 -0300

    Adicionando a parte do JavaScript
    Começando a parte lógica do site

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+let x = document.querySelector(".x"); // Em relação ao placar
+let o = document.querySelector(".o"); // Em relação ao placar
+let boxes = document.querySelectorAll(".box"); 
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+// Contador de Jogadas
+let player1 = 0;
+let player2 = 0;
+
+// adicionando um o evento de click aos boxes
+for(let i = 0; i < boxes.length; i++){
+
+    // quando alguém clica na caixa
+    boxes[i].addEventListener("click", function()){
+        
+    }
+}
